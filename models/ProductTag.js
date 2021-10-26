b2cf48d7e41187b024647688c6abb3d0bc29f899
Matt Weichel <maweiche@gmail.com>
2021-10-26 15:10:49 -0700

    defined columns for productTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,35 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //define product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: 'id'
+      }
+    },
+    //define tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
